1185904bb203e047d8aee8f2232cea34ddd21d6a
Jorge <jorgevc_87@outlook.com>
2026-04-24 21:11:45 -0500

    exercises

--- MyPlayground.playground/Pages/Page5.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Page5.xcplaygroundpage/Contents.swift
@@ -2,9 +2,26 @@
 
 import Foundation
 
+// protocol
+protocol SalaryProtocol {
+    func showMoney(name: String, money: Double)
+}
+
 // delegating tasks
-struct Salary {
+struct Salary : SalaryProtocol{
     func showMoney(name: String, money: Double) {
+        print("The salary of \(name) is \(money)")
+    }
+}
+
+struct BasicSalary: SalaryProtocol {
+    func showMoney(name: String, money: Double) {
+        
+        if (money > 4000) {
+            print("The salary is over the minimium")
+            return
+        }
+        
         print("The salary of \(name) is \(money)")
     }
 }
@@ -13,7 +30,7 @@ struct Employees {
     var name: String
     var money: Double
     
-    var delegate: Salary
+    var delegate: SalaryProtocol
     
     func generateReport() {
         delegate.showMoney(name: name, money: money)
@@ -22,6 +39,8 @@ struct Employees {
 
 let salary = Salary()
 var employee1 = Employees(name: "Tom", money: 4500, delegate: salary)
+
+employee1.delegate = BasicSalary()
 employee1.generateReport()
 
 
